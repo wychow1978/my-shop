dcadd9b1fc3fa2cf20fb21a14d26237f32d81b54
wychow1978 <wychow.1978@gmail.com>
2026-01-21 01:48:21 +0800

    Update products.js

--- products.js
+++ products.js
@@ -4,28 +4,28 @@ const allProducts = [
     {
         "id": "cup-001",
         "name": "极简陶瓷马克杯",
-        "price": 25,
+        "price": 999,
         "image": "product_images/cup.jpg",
         "active": true
     },
     {
         "id": "bag-002",
         "name": "城市漫游帆布包",
-        "price": 45,
+        "price": 999,
         "image": "product_images/bag.jpg",
         "active": true
     },
     {
         "id": "lamp-003",
         "name": "黄铜复古台灯",
-        "price": 89,
+        "price": 999,
         "image": "product_images/lamp.jpg",
         "active": true
     },
     {
         "id": "cup-002",
         "name": "小叮当杯",
-        "price": 19.9,
+        "price": 999.999,
         "image": "product_images/",
         "active": true
     }
