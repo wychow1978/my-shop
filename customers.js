dbb2dad41a92a641407e50ca50983a0f5cfbd07e
wychow1978 <wychow.1978@gmail.com>
2026-01-21 03:06:53 +0800

    Update customers

--- customers.js
+++ customers.js
@@ -1,2 +1,12 @@
 // customers Data
-const allCustomers = [];
+const allCustomers = [
+    {
+        "id": "cust-1768935989399",
+        "name": "Rain Ong",
+        "country_code": "+60",
+        "phone": "167119854",
+        "password": "12345",
+        "notes": "",
+        "history": []
+    }
+];
